956f91d9cafa04c0193d2cae5feec3f2b6338d47
Phillip Alday <me@phillipalday.com>
2021-02-08 14:14:57 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/MixedModelsGadfly.jl
@@ -0,0 +1,5 @@
+module MixedModelsGadfly
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MixedModelsGadfly
+using Test
+
+@testset "MixedModelsGadfly.jl" begin
+    # Write your tests here.
+end
